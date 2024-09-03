f2335eddd964ef3f19044f1e8ad28a2e64e66a43
Riley Champion <rchamp@Rileys-Laptop.local>
2024-09-03 01:26:45 -0400

    Create simple todo list api

--- main.go
+++ main.go
@@ -1,14 +1,140 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+type todo struct {
+	ID          int64  `json:"id"`
+	Description string `json:"description"`
+}
 
-	http.ListenAndServe(":80", nil)
+var todoList = []todo{
+	{ID: 0, Description: "test descriptions"},
+	{ID: 1, Description: "test descriptions2"},
+	{ID: 2, Description: "test descriptions3"},
+	{ID: 3, Description: "test descriptions4"},
+	{ID: 4, Description: "test descriptions5"},
+	{ID: 5, Description: "test descriptions6"},
+}
+
+var idCounter = 1
+
+func binarySearch(arr *[]todo, target int64) (*todo, error) {
+
+	var low float64 = 0
+	var high float64 = float64(len((*arr)))
+
+	for low <= high {
+		mid := int64(math.Floor((high + low) / 2))
+		if (*arr)[mid].ID == target {
+			return &(*arr)[mid], nil
+		} else if (*arr)[mid].ID < target {
+			low = float64(mid + 1)
+		} else if (*arr)[mid].ID > target {
+			high = float64(mid - 1)
+		}
+	}
+
+	return nil, errors.New("Error not able to find target")
+
+}
+
+func getAllTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todoList)
+}
+
+func addTodo(c *gin.Context) {
+	var newTodo todo
+
+	if err := c.BindJSON(&newTodo); err != nil {
+		fmt.Println("ERRROR")
+		return
+	}
+
+	todoList = append(todoList, newTodo)
+	c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func getTodoByID(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 36, 64)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "invalid id"})
+		return
+	}
+
+	foundTodo, err := binarySearch(&todoList, id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, (*foundTodo))
+}
+
+func deleteTodo(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 36, 64)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "invalid id"})
+		return
+	}
+
+	var deleteInstance *todo = nil
+	newArr := []todo{}
+
+	for _, item := range todoList {
+		if item.ID == id {
+			deleteInstance = &item
+			continue
+		}
+		newArr = append(newArr, item)
+	}
+
+	if deleteInstance == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "invalid id"})
+		return
+	}
+
+	todoList = newArr
+
+	c.IndentedJSON(http.StatusOK, (*deleteInstance))
+
+}
+
+func updateTodo(c *gin.Context) {
+	var updateTodo todo
+
+	if err := c.BindJSON(&updateTodo); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "internal server error"})
+		return
+	}
+
+	for ind, item := range todoList {
+		if item.ID == updateTodo.ID {
+			todoList[ind].Description = updateTodo.Description
+			c.IndentedJSON(http.StatusOK, todoList[ind])
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getAllTodos)
+	router.GET("/todo/:id", getTodoByID)
+	router.POST("/todo", addTodo)
+	router.DELETE("/todo/:id", deleteTodo)
+	router.PUT("/todo/:id", updateTodo)
+
+	router.Run("localhost:8080")
 }
